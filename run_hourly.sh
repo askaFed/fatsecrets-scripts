a26dd1f46ad37b29622c19a74086e9b03ddf4764
Anastasiia Fedorak <aska.fed@gmail.com>
2025-06-13 18:15:01 +0300

    Update script

--- run_hourly.sh
+++ run_hourly.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
 DATE=$(date +"%Y-%m-%d_%H-%M")
 
 SCRIPTS=(
@@ -8,13 +10,18 @@ SCRIPTS=(
   "./scripts/fetch-fs-data/fetch_weight.py"
 )
 
+source "$SCRIPT_DIR/venv/bin/activate"
+
 for SCRIPT in "${SCRIPTS[@]}"; do
   SCRIPT_NAME=$(basename "$SCRIPT" .py)
 
-  LOG_DIR="./output/logs/$SCRIPT_NAME"
+  LOG_DIR="$SCRIPT_DIR/output/logs/$SCRIPT_NAME"
   mkdir -p "$LOG_DIR"
 
-  python3 "$SCRIPT" >> "$LOG_DIR/${SCRIPT_NAME}_$DATE.log" 2>&1
+  python "$SCRIPT_DIR/$SCRIPT" >> "$LOG_DIR/${SCRIPT_NAME}_$DATE.log" 2>&1
+
 done
 
-find ./output/logs/ -type f -name "*.log" -mtime +7 -delete
+deactivate
+
+find "$SCRIPT_DIR/output/logs/" -type f -name "*.log" -mtime +7 -delete
