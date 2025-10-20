236ed973f74c3d829c788d1385cf8b4836ee8680
Anastasiia Fedorak <anastasiia.fedorak@gameloft.com>
2025-10-20 16:12:29 +0300

    share_meals.sh: Accept args

--- share_meals.sh
+++ share_meals.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-
 DATE=$(date +"%Y-%m-%d_%H-%M")
-
 SCRIPT="./scripts/fetch-fs-data/share_meals.py"
+
 source "$SCRIPT_DIR/venv/bin/activate"
 pip install -r "$SCRIPT_DIR/requirements.txt"
 
 SCRIPT_NAME=$(basename "$SCRIPT" .py)
-
 LOG_DIR="$SCRIPT_DIR/output/logs/$SCRIPT_NAME"
 mkdir -p "$LOG_DIR"
 
-python "$SCRIPT_DIR/$SCRIPT" >> "$LOG_DIR/${SCRIPT_NAME}_$DATE.log" 2>&1
+# Pass all command line arguments to the Python script
+python "$SCRIPT_DIR/$SCRIPT" "$@" >> "$LOG_DIR/${SCRIPT_NAME}_$DATE.log" 2>&1
 
 deactivate
